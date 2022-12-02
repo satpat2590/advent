99521cf06390abd88c58c17bd4f95b4b66d8b10a
Satyam Patel <67593031+satpat2590@users.noreply.github.com>
2022-12-02 02:39:55 -0500

    erased i/o clutter

--- day2/go.cc
+++ day2/go.cc
@@ -6,17 +6,33 @@
 #include <deque> 
 
 
+/*
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. 
+"Anyway, the second column says how the round needs to end: 
+X means you need to lose, 
+Y means you need to end the round in a draw, and 
+Z means you need to win. 
+Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. 
+The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. 
+This gives you a score of 1 + 3 = 4.
+
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, 
+what would your total score be if everything goes exactly according to your strategy guide?
+*/
+
+
 int checkMate(std::pair<int, int> match) {
-    //match.second = 1 = lose = 
-    //match.second = 2 = draw = 
-    //match.second = 3 = win = 
-
-    //1 = rock
-    //2 = paper
-    //3 = scissor
-
-    //win = 6
-    printf("\n%d\n%d\n\n", match.first, match.second);
     switch(match.first) {
         case 1:
             switch(match.second) {
@@ -52,7 +68,6 @@ int checkMate(std::pair<int, int> match) {
                     return 1 + 6;
             }
     }
-
     return -1; 
 }
 
@@ -61,7 +76,6 @@ int checkMate(std::pair<int, int> match) {
 std::pair<int, int> checkBoth(char first, char second) {
     int retFirst = 0;
     int retSecond = 0;
-    printf("\n%c %c\n", first, second);
     if (first == 'A') 
         retFirst = 1; 
     
@@ -79,8 +93,6 @@ std::pair<int, int> checkBoth(char first, char second) {
     
     if (second == 'Z')
         retSecond = 3; 
-
-    printf("\n%d %d\n", retFirst, retSecond);
 
     return std::make_pair(retFirst, retSecond);
 }
